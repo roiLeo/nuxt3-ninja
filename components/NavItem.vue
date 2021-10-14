b02ecc3ced755e8126b473327765987fe304e889
roiLeo <medina.leo42@gmail.com>
2021-10-14 19:28:39 +0200

    🔧 working on router

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -1,11 +1,25 @@
 <template>
-	<a
-		:href="href"
-		:class="isActive ? 'font-semibold text-gray-800 dark:text-gray-200' : 'font-normal text-gray-600 dark:text-gray-400'"
-        class="hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all"
-      >
-        <span className="capsize">{{text}}</span>
-      </a>
+	<NuxtLink
+		:to="href"
+		:class="
+			isActive
+				? 'font-semibold text-gray-800 dark:text-gray-200'
+				: 'font-normal text-gray-600 dark:text-gray-400'
+		"
+		class="
+			hidden
+			md:inline-block
+			p-1
+			sm:px-3
+			sm:py-2
+			rounded-lg
+			hover:bg-gray-200
+			dark:hover:bg-gray-800
+			transition-all
+		"
+	>
+		<span className="capsize">{{ text }}</span>
+	</NuxtLink>
 </template>
 
 <script lang="ts">
@@ -13,19 +27,28 @@ import { computed, defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default defineComponent({
-	props: [
-		'href',
-		'text'
-	],
-	data: () => ({isActive: false}),
+	props: ['href', 'text'],
+	watch: {
+		async $route(to, from) {
+			this.isActive =
+				(this.href === '/' && to.name === 'index') ||
+				to.name === this.href.replace('/', '')
+		},
+	},
+	data: () => ({ isActive: false }),
 	setup() {
 		const route = useRoute()
 		const routeName = computed(() => route.name)
-		return { routeName }
+		const activeRouteName = ref(routeName)
+		return { activeRouteName, routeName }
 	},
 	created() {
-		this.isActive = (this.href === '/' && this.routeName === 'index') ||
- 			(this.routeName === this.href.replace('/', '') )
-	}
+		this.isActive =
+			(this.href === '/' && this.routeName === 'index') ||
+			this.routeName === this.href.replace('/', '')
+	},
 })
 </script>
+<style scoped>
+.router-link-active .router-link-exact-active {}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-	buildModules: [
-		'nuxt-windicss',
-	],
+	buildModules: ['nuxt-windicss'],
 	css: [
 		'virtual:windi.css',
 		// '@/assets/css/main.css'
 	],
+	router: {
+		// Not working
+		linkActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
+		linkExactActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
+	},
+	ssr: false,
 })
