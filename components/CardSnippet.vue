c7ad0dde1d6c0083f5b3074ec76569f3dce78b82
roiLeo <medina.leo42@gmail.com>
2021-10-18 14:14:14 +0200

    🏗  wip snippets page

---
+++ components/CardSnippet.vue
@@ -0,0 +1,35 @@
+<template>
+	<a
+		class="
+			border border-grey-200
+			dark:border-gray-800
+			rounded
+			p-4
+			w-full
+			bg-white
+			dark:bg-gray-900
+			hover:ring-1
+			ring-gray-300
+			hover:bg-gray-200
+			dark:hover:bg-gray-800
+			transition-all
+		"
+	>
+		<!-- <img alt="text alternative" src="/nuxt-icon.png" class="rounded-full" /> -->
+		<h3
+			class="
+				text-lg
+				font-bold
+				text-left
+				mt-2
+				text-gray-900
+				dark:text-gray-100
+			"
+		>
+			This is a title
+		</h3>
+		<p class="mt-1 text-gray-700 dark:text-gray-400">
+			This ain't a description
+		</p>
+	</a>
+</template>
---
+++ pages/snippets/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<div
+		class="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16"
+	>
+		<h1
+			class="
+				mb-4
+				text-3xl
+				font-bold
+				tracking-tight
+				text-black
+				md:text-5xl
+				dark:text-white
+			"
+		>
+			Code Snippets
+		</h1>
+		<p class="mb-4 text-gray-600 dark:text-gray-400">
+			These are a collection of code snippets I've used in the past and
+			saved. It include anything from random CSS snippets to
+			Node.js scripts.
+		</p>
+		<div class="grid w-full grid-cols-1 gap-4 my-2 mt-4 sm:grid-cols-2">
+			<CardSnippet />
+			<CardSnippet />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+
+	},
+})
+</script>
