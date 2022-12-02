8dabd8bdcb0ed85f2d33fa7f0f939962578e8d97
roiLeo <medina.leo42@gmail.com>
2022-12-02 15:28:38 +0100

    ✨ nuxt 3 release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,14 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-	buildModules: [
-		'nuxt-windicss',
-		'@vueuse/core/nuxt'
-	],
+	buildModules: ['@vueuse/nuxt'],
+	modules: ['nuxt-windicss'],
 	css: [
-		'virtual:windi.css',
+		// 'virtual:windi.css'
 		// '@/assets/css/main.css'
 	],
 	router: {
 		// Not working
 		linkActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
-		linkExactActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
-	},
+		linkExactActiveClass: 'font-semibold text-gray-800 dark:text-gray-200'
+	}
 	// ssr: false,
 })
