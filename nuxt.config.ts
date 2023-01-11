dbc3cd712f9a3d10b2f2cc04300bb5e326b8a3e6
roiLeo <medina.leo42@gmail.com>
2023-01-11 14:57:06 +0100

    ⬆️ upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 export default defineNuxtConfig({
-	buildModules: ['@vueuse/nuxt'],
-	modules: ['nuxt-windicss'],
-	css: [
-		// 'virtual:windi.css'
-		// '@/assets/css/main.css'
-	],
+	modules: ['nuxt-windicss', '@vueuse/nuxt'],
 	router: {
-		// Not working
-		linkActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
-		linkExactActiveClass: 'font-semibold text-gray-800 dark:text-gray-200'
+		options: {
+			linkActiveClass: 'font-semibold text-gray-800 dark:text-gray-200',
+			linkExactActiveClass: 'font-semibold text-gray-800 dark:text-gray-200'
+		}
 	}
 	// ssr: false,
 })
